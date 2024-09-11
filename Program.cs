5811fed0a6c3b73cc43336f7bf12328dfea28238
igorferreira10 <igorferrira647@gmail.com>
2024-09-11 15:34:52 -0300

    Desafio concluídogit statusgit statusgit statusgit status!

--- Program.cs
+++ Program.cs
@@ -1,3 +1,21 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+class Program
+{
+    static void Main(string[] args)
+    {
+    //Testando o Nokia
+    Smartphone nokia = new Nokia("123456789", "Nokia 3310", "1111111111111", 64);
+    nokia.ligar();
+    nokia.ReceberLigacao();
+    nokia.InstalarAplicativo("WhatsApp");
+
+    Console.WriteLine();
+
+    //Testando iPhone
+    Smartphone iphone = new Iphone("987654321","iPhone 13", "2222222222222", 256 );
+    iphone.Ligar();
+    iphone.ReceberLigacao();
+    iphone.InstalarAplicativo("Instagram");
+    }
+}
